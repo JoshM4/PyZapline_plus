4dadfc38889259fe8e452a6b2a329926f0c552fc
Josh Mosse-Robinson <josh.mr@gmail.com>
2025-05-22 18:28:00 +1000

    Created required files for use as a package

---
+++ pyZapline_plus/__init__.py
@@ -0,0 +1,7 @@
+"""
+PyZaplinePlus - Python adaptation of the Zapline-plus library for EEG noise removal.
+"""
+
+from .pyzaplineplus import zapline_plus, PyZaplinePlus
+
+__all__ = ['zapline_plus', 'PyZaplinePlus'] 
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyzaplineplus",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-learn",
+        "matplotlib",
+    ],
+    python_requires=">=3.13",
+) 
